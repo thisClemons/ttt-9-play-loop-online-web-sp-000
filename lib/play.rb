b4ec38c869fe4db7cf425eec26176b432908c737
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-08 23:31:14 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,14 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  turns = 0
+  loop do
+    turn(board)
+    turns += 1
+    if turns >= 9
+      break
+    end
+  end
+
+end
